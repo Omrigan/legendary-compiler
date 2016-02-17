de9b10a9e66bf3326df7f2ebd5b7e43a98fb0a49
Omrigan <omrigann@gmail.com>
2016-02-17 22:56:21 +0300

    Comments added

--- main.cpp
+++ main.cpp
@@ -17,7 +17,8 @@ unordered_set<string> keywords = {
 
 unordered_set<string> signs = {
         "{",
-        "}"
+        "}",
+        "."
 };
 
 unordered_set<string> operators = {
@@ -32,23 +33,74 @@ struct lexem {
 
 
 };
-bool isFloatLiteral (string s){
+
+bool isFloatLiteral(string s) {
     regex re("(\\d*\\.\\d+|\\d+\\.\\d*)f?|\\d+f");
     return regex_match(s.c_str(), re);
 }
 
+bool isIntLiteral(string s) {
+    regex re("\\d+");
+    return regex_match(s.c_str(), re);
+}
+
+bool isStringLiteral(string s) {
+    regex re("\".*\"");
+    return regex_match(s.c_str(), re);
+}
+
+bool isId(string s) {
+    regex re("[A-Za-z_]+[A-Za-z0-9_-]*");
+    return regex_match(s.c_str(), re);
+}
+
+bool isShortComment(string s) {
+    regex re("//.*");
+    return regex_match(s.c_str(), re);
+}
+
+bool isLongCommentBegiging(string s) {
+    regex re("/\\*.*");
+    return regex_match(s.c_str(), re);
+}
+
+bool isLongCommentEnding(string s) {
+    regex re(".*\\*/");
+    return regex_match(s.c_str(), re);
+}
+bool isLineEnding(string s){
+    return s=="\n";
+}
 
 vector<lexem> analysis(vector<string> strings) {
     vector<lexem> out;
+    bool short_comment = false;
+    bool long_comment = false;
     for (string s : strings) {
         lexem l;
         l.s = s;
-        if (operators.count(s) > 0) l.t = types::OPERATOR;
-        else if (signs.count(s) > 0) l.t = types::SIGN;
-        else if (keywords.count(s) > 0) l.t = types::KEYWORD;
-        else if (isFloatLiteral(s)) l.t = types::FLOAT_LITERAL;
-        else l.t = types::OTHER;
-        out.push_back(l);
+        if (isShortComment(s))
+            short_comment = true;
+        if(isLongCommentBegiging(s))
+            long_comment = true;
+
+        if(isLineEnding(s))
+            short_comment = false;
+        else if (!long_comment && !short_comment) {
+            if (operators.count(s) > 0) l.t = types::OPERATOR;
+            else if (signs.count(s) > 0) l.t = types::SIGN;
+            else if (keywords.count(s) > 0) l.t = types::KEYWORD;
+            else if (isFloatLiteral(s)) l.t = types::FLOAT_LITERAL;
+            else if (isStringLiteral(s)) l.t = types::STRING_LITERAL;
+            else if (isIntLiteral(s)) l.t = types::INT_LITERAL;
+            else if (isId(s)) l.t = types::ID;
+            else l.t = types::OTHER;
+            out.push_back(l);
+        }
+        if(isLongCommentEnding(s))
+            long_comment = false;
+
+
     }
     return out;
 
@@ -77,10 +129,10 @@ void run() {
         getline(cin, s);
         vector<string> tmp = spilt(s);
         for (string s2 : tmp) {
-            transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
             if (s2.size() > 0)
                 strings.push_back(s2);
         }
+        strings.push_back("\n");
     }
     vector<lexem> lexems = analysis(strings);
     for (lexem l : lexems) {
